2915ae8bbc01679c670f76d7b573db6fce88996d
Sophia See <sophiasee11@gmail.com>
2025-02-24 13:55:08 +0800

    updated mobile menu

--- components/MobileMenu.tsx
+++ components/MobileMenu.tsx
@@ -3,12 +3,61 @@
 import Image from 'next/image'
 import React from 'react'
 import Divider from './Divider';
-import Link from 'next/link';
+import Link, { LinkProps } from 'next/link';
 import Button from './Button';
 import CirclePattern from './CirclePattern';
+import { usePathname } from 'next/navigation';
+
+interface NavLinkProps extends LinkProps {
+  children?: React.ReactNode;
+}
+
+function NavLink ({children, ...props}: NavLinkProps) {
+  return (
+    <Link 
+      className='opacity-70 py-4' 
+      {...props}
+    >
+      {children}
+    </Link>
+  )
+}
 
 export default function MobileMenu() {
+  const pathname = usePathname();
   const [isOpen, setIsOpen] = React.useState(false);
+  const menuRef = React.useRef<HTMLDivElement | null>(null);
+
+  React.useEffect(() => {
+    function handleClickOutside(event: MouseEvent) {
+      if (menuRef.current && !menuRef.current.contains(event.target as Node)) {
+        setIsOpen(false);
+      }
+    }
+
+    // Add event listener
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      // Cleanup event listener on unmount
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
+  React.useEffect(() => {
+    setIsOpen(false);
+  }, [pathname]);
+
+  React.useEffect(() => {
+    if (isOpen) {
+      document.body.style.overflow = "hidden"
+    } else {
+      document.body.style.overflow = ""
+    }
+
+    return () => {
+      document.body.style.overflow = ""
+    }
+  }, [isOpen]);
 
   return (
     <div>
@@ -28,6 +77,7 @@ export default function MobileMenu() {
         `}
       >
         <div 
+          ref={menuRef}
           className={`
             relative
             h-full w-[298px] ml-auto
@@ -51,12 +101,12 @@ export default function MobileMenu() {
                 setIsOpen(false)
               }}
             />
-            <Divider mode={"light"} className='opacity-15 mt-6 mb-[43px]'/>
-            <nav className='flex flex-col gap-8 items-center font-bold text-[20px] text-link-water-white w-[250px]'>
-              <Link className='opacity-70' href={"/pricing"}>Pricing</Link>
-              <Link className='opacity-70' href={"/about"}>About</Link>
-              <Link className='opacity-70' href={"/contact"}>Contact</Link>      
-              <Button className='w-full'>Schedule a Demo</Button>      
+            <Divider mode={"light"} className='opacity-15 mt-6 mb-[calc(43px-16px)]'/>
+            <nav className='flex flex-col items-center font-bold text-[20px] text-link-water-white w-[250px]'>
+              <NavLink href={"/pricing"}>Pricing</NavLink>
+              <NavLink href={"/about"}>About</NavLink>
+              <NavLink href={"/contact"}>Contact</NavLink>      
+              <Button className='w-full mt-4'>Schedule a Demo</Button>      
             </nav>
           </div>          
         </div>
