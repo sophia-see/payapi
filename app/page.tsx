261fb8e23ec849e27d6c69cde0b7786987b4ca12
Sophia See <sophiasee11@gmail.com>
2025-02-24 15:13:04 +0800

    updated circle background tablet

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
       {/* <CirclePattern parentClass="inset-0 -translate-y-[60%]"/> */}
       <CirclePattern 
         parentClass='absolute top-0 right-0 -z-10 overflow-hidden'
-        childClass='w-screen h-screen -translate-y-[60%] h-full place-self-end ml-auto'
+        childClass='w-screen h-screen -translate-y-[60%] md:-translate-y-[70%] h-full place-self-end ml-auto'
       />
 
       {/* temp */}
