d1c80c14f2f7ae9bcd2be99078f8cedc76078da0
Sophia See <sophiasee11@gmail.com>
2025-02-25 13:39:34 +0800

    updated max width footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -27,45 +27,53 @@ export default function Footer() {
   return (
     <footer 
       className={`
-        pt-[41px] pb-[55px] md:py-8 md:px-10 lg:px-[115px] xl-1440:px-[165px]
         bg-mirage-blue 
-        flex flex-col items-center gap-10 md:flex-row md:gap-16
-        relative overflow-hidden
+        
       `}
     >
-      <Logo mode='dark'/>
-      <div className='flex flex-col items-center gap-[31px] z-30 md:flex-row md:gap-10'>
-        <NavLink href={"/pricing"}>Pricing</NavLink>
-        <NavLink href={"/about"}>About</NavLink>
-        <NavLink href={"/contact"}>Contact</NavLink>
+      <div 
+        className={`
+        lg:max-w-[1110px] lg:mx-auto
+        pt-[41px] pb-[55px] md:py-8 md:mx-10
+        flex flex-col items-center gap-10 md:flex-row md:gap-16
+        relative overflow-hidden
+        `}
+      >
+        <Logo mode='dark'/>
+        <div className='flex flex-col items-center gap-[31px] z-30 md:flex-row md:gap-10'>
+          <NavLink href={"/pricing"}>Pricing</NavLink>
+          <NavLink href={"/about"}>About</NavLink>
+          <NavLink href={"/contact"}>Contact</NavLink>
+        </div>
+        <div className='flex items-center gap-6 z-30 md:flex-1 md:place-content-end'>
+          <Image
+            src={"/assets/shared/desktop/facebook.svg"}
+            alt='facebook icon'
+            width={24}
+            height={24}
+            className="cursor-pointer filter invert brightness-0 hover:filter-none"
+          />
+          <Image
+            src={"/assets/shared/desktop/twitter.svg"}
+            alt='twitter icon'
+            width={24}
+            height={24}
+            className="cursor-pointer filter invert brightness-0 hover:filter-none"
+          />
+          <Image
+            src={"/assets/shared/desktop/linkedin.svg"}
+            alt='linkedin icon'
+            width={24}
+            height={24}
+            className="cursor-pointer filter invert brightness-0 hover:filter-none"
+          />
+        </div>
+        <CirclePattern 
+          parentClass='w-full absolute bottom-0 h-[185px] overflow-hidden'
+          childClass='absolute bottom-0 left-0 right-0 translate-y-[calc(780px-185px)] md:translate-x-[55%]'
+        />  
       </div>
-      <div className='flex items-center gap-6 z-30 md:flex-1 md:place-content-end'>
-        <Image
-          src={"/assets/shared/desktop/facebook.svg"}
-          alt='facebook icon'
-          width={24}
-          height={24}
-          className="cursor-pointer filter invert brightness-0 hover:filter-none"
-        />
-        <Image
-          src={"/assets/shared/desktop/twitter.svg"}
-          alt='twitter icon'
-          width={24}
-          height={24}
-          className="cursor-pointer filter invert brightness-0 hover:filter-none"
-        />
-        <Image
-          src={"/assets/shared/desktop/linkedin.svg"}
-          alt='linkedin icon'
-          width={24}
-          height={24}
-          className="cursor-pointer filter invert brightness-0 hover:filter-none"
-        />
-      </div>
-      <CirclePattern 
-        parentClass='w-full absolute bottom-0 h-[185px] overflow-hidden'
-        childClass='absolute bottom-0 left-0 right-0 translate-y-[calc(780px-185px)] md:translate-x-[55%]'
-      />  
+      
     </footer>
   )
 }
